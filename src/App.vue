ebbabb4a9316458363ead6d85ce5f9634b247dab
GiL <iamleeyounggil@gmail.com>
2018-02-05 11:40:35 -0500

    Add mutation type and sidbar pages

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div :class="$style.container">
+
+    </div>
+    <sidebar/>
+    <sidebar-toggle/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Sidebar from './components/sidebar.vue'
+  import SidebarToggle from './components/sidebarToggle.vue'
+  export default {
+    name: 'app',
+    components: {
+      Sidebar, SidebarToggle
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root{
+    --accent-color: #FFCB08;
+    --primary-color: #820263;
+    --dark-color: #2E294E;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  *{
+    box-sizing: border-box;
+  }
+</style>
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style module>
+  .container{
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: var(--primary-color);
+  }
 </style>
